8254574f763a491629cc0181f7c0c0616130b581
laanayam333 <laanayam333@gmail.com>
2019-12-18 16:20:20 +0100

    Updates

--- precache-manifest.f05766de2072bf42e12dabc53b820b8d.js
+++ precache-manifest.f05766de2072bf42e12dabc53b820b8d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1e89eb6cb8c09d183f195fb84e45baf",
+    "revision": "19778e6a92467decebb8aea723d6d4b0",
     "url": "/seeing-red/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/seeing-red/static/css/2.06565635.chunk.css"
   },
   {
-    "revision": "45fbbe6847ec860f6236",
+    "revision": "d9e70d685adf144fbe94",
     "url": "/seeing-red/static/css/main.c5d12e20.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/seeing-red/static/js/2.cad7161f.chunk.js"
   },
   {
-    "revision": "45fbbe6847ec860f6236",
-    "url": "/seeing-red/static/js/main.feab7f81.chunk.js"
+    "revision": "d9e70d685adf144fbe94",
+    "url": "/seeing-red/static/js/main.4eb43834.chunk.js"
   },
   {
     "revision": "4f85e098c46e42e3033d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seeing-red/precache-manifest.18180636db3c867df5e119f5f39dd827.js"
+  "/seeing-red/precache-manifest.f05766de2072bf42e12dabc53b820b8d.js"
 );
 
 self.addEventListener('message', (event) => {
