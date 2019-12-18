22b07facc717d562815d73a54bdea8bd2c954207
laanayam333 <laanayam333@gmail.com>
2019-12-18 15:16:07 +0100

    Updates

---
+++ precache-manifest.afd74569e7af6ac33cb38007babaa6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92caffff077cf5bda6e5d739129a75bc",
+    "url": "/seeing-red/index.html"
+  },
+  {
+    "revision": "f999b6a6b93e760b0889",
+    "url": "/seeing-red/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "55e5ecd9cccea0270dd5",
+    "url": "/seeing-red/static/css/main.c5d12e20.chunk.css"
+  },
+  {
+    "revision": "f999b6a6b93e760b0889",
+    "url": "/seeing-red/static/js/2.9a908ddb.chunk.js"
+  },
+  {
+    "revision": "55e5ecd9cccea0270dd5",
+    "url": "/seeing-red/static/js/main.8f41555c.chunk.js"
+  },
+  {
+    "revision": "4f85e098c46e42e3033d",
+    "url": "/seeing-red/static/js/runtime-main.38ee00b1.js"
+  },
+  {
+    "revision": "fcb140fa40e44d3659d09133d7c10f9b",
+    "url": "/seeing-red/static/media/hero-video.fcb140fa.mp4"
+  }
+]);
